bbb1161700c7927363c9eb324025d0b2cc98d327
Davi Ferreira <ferreira.dfs.davi@gmail.com>
2026-04-08 21:27:36 -0300

    mudança no banco

--- src/db/connection.js
+++ src/db/connection.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || "127.0.0.1",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "nail_designer",
+    host: process.env.DB_HOST || "45.143.7.175",
+    user: process.env.DB_USER || "davi",
+    password: process.env.DB_PASSWORD || "Davi@159",
+    database: process.env.DB_NAME || "beatriznails",
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
